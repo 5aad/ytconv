fe3c698cdd9074b55df4252cff34da6054daef22
Saad <saad_anwer@live.com>
2020-05-18 16:37:09 +0500

    ss

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ const path = require("path");
 const fs = require("fs");
 const ytdl = require("ytdl-core");
 const ffmpeg = require("fluent-ffmpeg");
-const https = require('https');
+const https = require("https");
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
@@ -14,7 +14,7 @@ router.get("/", function (req, res, next) {
 
 router.post("/", async (req, res) => {
   const { ytlink, options } = req.body;
-  const lang = 'en';
+  const lang = "en";
 
   if (options === "mp4") {
     try {
@@ -38,8 +38,7 @@ router.post("/", async (req, res) => {
             console.log("Saving to", output);
             const file = fs.createWriteStream(output);
             https.get(track.baseUrl, (res) => {
-              res.pipe(file)
-
+              res.pipe(file);
             });
           } else {
             console.log("Could not find captions for", lang);
@@ -61,15 +60,34 @@ router.post("/", async (req, res) => {
   } else if (options === "mp3") {
     try {
       let title = "audio";
-      await ytdl.getBasicInfo(
-        ytlink,
-        {
-          format: "mp4",
-        },
-        (err, info) => {
-          title = info.player_response.videoDetails.title;
+      await ytdl.getBasicInfo(ytlink, (err, info) => {
+        if (err) throw err;
+        title = info.player_response.videoDetails.title;
+        const tracks =
+          info.player_response.captions.playerCaptionsTracklistRenderer
+            .captionTracks;
+        if (tracks && tracks.length) {
+          console.log(
+            "Found captions for",
+            tracks.map((t) => t.name.simpleText).join(", ")
+          );
+          const track = tracks.find((t) => t.languageCode === lang);
+          if (track) {
+            console.log("Retrieving captions:", track.name.simpleText);
+            console.log("URL", track.baseUrl);
+            const output = `${info.title}.${track.languageCode}.xml`;
+            console.log("Saving to", output);
+            const file = fs.createWriteStream(output);
+            https.get(track.baseUrl, (res) => {
+              res.pipe(file);
+            });
+          } else {
+            console.log("Could not find captions for", lang);
+          }
+        } else {
+          console.log("No captions found for this video");
         }
-      );
+      });
 
       res.header("Content-Disposition", `attachment; filename="${title}.mp3"`);
 
